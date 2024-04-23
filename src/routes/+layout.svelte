b20a98589f35975c0a66921bcf94790d321c9d5b
Ken Daniele Ramiscal <code.ramiscalken@gmail.com>
2024-04-23 06:38:49 +0000

    create layout for navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { LayoutData } from './$types';
+    
+    export let data: LayoutData;
+</script>
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/add">Add item</a>
+</nav>
+<main>
+    <slot></slot>
+</main>
