bdf0ac0d3a4472b53eb1dd60195b6d2f7b20b345
Ken Daniele Ramiscal <code.ramiscalken@gmail.com>
2024-04-23 23:18:24 +0000

    add font assets

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,30 @@
 	import '../app.css';
 	import * as Menubar from '$lib/components/ui/menubar';
 	import { buttonVariants } from '$lib/components/ui/button';
-	import { page } from "$app/stores"
+	import { page } from '$app/stores';
 </script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://rsms.me/" />
+	<link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+</svelte:head>
 <Menubar.Root>
 	<Menubar.Menu>
-		<a href="/" class={buttonVariants({variant: $page.route.id === "/" ? "default" : "ghost"})}>Home</a>
-		<a href="/add" class={buttonVariants({variant: $page.route.id === "/add" ? "default" : "ghost"})}>Add item</a>
+		<div class="m-auto">
+			<a href="/" class={buttonVariants({ variant: $page.route.id === '/' ? 'default' : 'ghost' })}
+				>Home</a
+			>
+			<a
+				href="/add"
+				class={buttonVariants({ variant: $page.route.id === '/add' ? 'default' : 'ghost' })}
+				>Add</a
+			>
+			<a
+				href="/quiz"
+				class={buttonVariants({ variant: $page.route.id === '/quiz' ? 'default' : 'ghost' })}
+				>Quiz</a
+			>
+		</div>
 	</Menubar.Menu>
 </Menubar.Root>
 <main class="p-4">
