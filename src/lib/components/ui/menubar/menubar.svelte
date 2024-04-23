6fd10c7e24cc765a16607a684e38a1949d89d379
Ken Daniele Ramiscal <code.ramiscalken@gmail.com>
2024-04-23 22:22:27 +0000

    add components from shadcn-svelte

---
+++ src/lib/components/ui/menubar/menubar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Menubar as MenubarPrimitive } from "bits-ui";
+	import { cn } from "$lib/utils.js";
+
+	type $$Props = MenubarPrimitive.Props;
+
+	let className: $$Props["class"] = undefined;
+	export { className as class };
+</script>
+
+<MenubarPrimitive.Root
+	class={cn(
+		"flex items-center space-x-3 rounded-md border bg-background py-1 px-3 shadow-sm",
+		className
+	)}
+>
+	<slot />
+</MenubarPrimitive.Root>
