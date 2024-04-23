1f417f53acfb3b38ee700da96c140a0f719600a5
Ken Daniele Ramiscal <code.ramiscalken@gmail.com>
2024-04-23 06:37:07 +0000

    serialized the _id to avoid error

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,6 +2,20 @@ import { Flashcard } from '$lib/db';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-	const flashcards = await Flashcard.find({});
-	return { flashcards };
+	const flashcards = (await Flashcard.find({}, {
+		question:true,
+		answer: true,
+		subject:true
+	})).map((item) => {
+		return {
+			question: item.question,
+			answer: item.answer,
+			subject: item.subject,
+			_id: item._id.toString()
+		}
+	})
+	console.log(flashcards)
+	return {
+		flashcards
+	};
 }) satisfies PageServerLoad;
