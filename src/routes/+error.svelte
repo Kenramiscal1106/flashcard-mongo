2f3b1a12fd57af0572d17af1d9b8c7c46953a64c
Ken Daniele Ramiscal <code.ramiscalken@gmail.com>
2024-04-23 23:16:03 +0000

    added custom error route

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+</script>
+
+<div class="max-w-2xl m-auto">
+    <h1 class="text-center">{$page.status}: {$page.error?.message}</h1>
+</div>
