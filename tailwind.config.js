272f14c755cd7abef3feecc230fc8cee510689dc
Ken Daniele Ramiscal <code.ramiscalken@gmail.com>
2024-04-24 22:42:32 +0000

    add backface visibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
 import { fontFamily } from "tailwindcss/defaultTheme";
 
+const plugin = require('tailwindcss/plugin')
+
+const backfaceVisibility = plugin(function ({ addUtilities }) {
+	addUtilities({
+		'.backface-visible': {
+			'backface-visibility': 'visible',
+			'-moz-backface-visibility': 'visible',
+			'-webkit-backface-visibility': 'visible',
+			'-ms-backface-visibility': 'visible'
+		},
+		'.backface-hidden': {
+			'backface-visibility': 'hidden',
+			'-moz-backface-visibility': 'hidden',
+			'-webkit-backface-visibility': 'hidden',
+			'-ms-backface-visibility': 'hidden'
+		}
+	})
+});
 /** @type {import('tailwindcss').Config} */
 const config = {
 	darkMode: ["selector"],
 	content: ["./src/**/*.{html,js,svelte,ts}"],
 	safelist: ["dark"],
+	plugins: [
+		backfaceVisibility,
+	],
 	theme: {
 		container: {
 			center: true,
