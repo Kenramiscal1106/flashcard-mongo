8aed908402e47ea15f62e5ac757f39d8b7c56c51
Ken Daniele Ramiscal <code.ramiscalken@gmail.com>
2024-05-01 05:48:15 +0000

    add logic for editing card

--- src/lib/components/Flashcard.svelte
+++ src/lib/components/Flashcard.svelte
@@ -1,40 +1,72 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
 	import type { FlashCard } from '$lib/db';
-	export let flashcard: FlashCard;
-	let open = false;
+
 	import CardTitle from './ui/card/card-title.svelte';
 	import Card from './ui/card/card.svelte';
 	import CardDescription from './ui/card/card-description.svelte';
 	import CardHeader from './ui/card/card-header.svelte';
 	import CardFooter from './ui/card/card-footer.svelte';
+	import CardContent from './ui/card/card-content.svelte';
+
 	import Button from './ui/button/button.svelte';
 	import { fade } from 'svelte/transition';
 	import { cn } from '$lib/utils';
-	import CardContent from './ui/card/card-content.svelte';
+	// import { modalStore, type ModalState } from '$lib/stores';
+
+	export let flashcard: FlashCard;
+	export let quiz: boolean;
+
+	let open = false;
+	function fireAction(type: ModalState['type']) {
+		return (e:Event) => {
+			e.stopPropagation()
+		};
+	}
 </script>
 
 <div class="flex-[0_0_20rem] relative pers-10 min-h-[27rem] card">
-	<Card class={cn("z-10 duration-200 absolute w-full h-full top-0 left-0 flex flex-col")} {open} on:click={() => open = true}>
+	<Card
+		class={cn('z-10 duration-200 absolute w-full h-full top-0 left-0 flex flex-col')}
+		{open}
+		on:click={() => (open = true)}
+	>
 		<CardHeader>
 			<CardDescription class="text-center text-neutral-600 dark:text-neutral-300"
 				>{flashcard.subject}</CardDescription
 			>
 		</CardHeader>
 		<CardContent>
-			<CardTitle tag="h2" class="text-center"><span in:fade={{duration: 175}}>{flashcard.question}</span></CardTitle>
+			<CardTitle tag="h2" class="text-center"
+				><span in:fade={{ duration: 175 }}>{flashcard.question}</span></CardTitle
+			>
 		</CardContent>
+		{#if !quiz}
+		<CardFooter>
+			<Button on:click={fireAction("flashcard-update")}>Update card</Button>
+			<Button on:click={fireAction("flashcard-delete")}>Delete card</Button>
+			<Button on:click={fireAction("update-box")}>update box</Button>
+		</CardFooter>
+		{/if}
 	</Card>
+	<!-- svelte-ignore a11y-no-static-element-interactions -->
 	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<div class={cn(!open ? "rotate-card" : "rotate-default", "duration-200 absolute w-full h-full top-0 right-0 backface-hidden border bg-card shadow p-6 rounded-xl flex flex-col justify-center")} on:click={() => open = false}>
+	<div
+		class={cn(
+			!open ? 'rotate-card' : 'rotate-default',
+			'duration-200 absolute w-full h-full top-0 right-0 backface-hidden border bg-card shadow p-6 rounded-xl flex flex-col justify-center'
+		)}
+		on:click={() => (open = false)}
+	>
 		<h4 class="text-center">{flashcard.answer}</h4>
 	</div>
 </div>
+
 <style>
 	* {
 		box-sizing: border-box;
 	}
-	.rotate-card, .rotate-default {
+	.rotate-card,
+	.rotate-default {
 		transition: transform 0.5s ease;
 	}
 	.card {
