e7d8d05adb3ce2bbea5391bca3433fc17b4a3142
Ken Daniele Ramiscal <code.ramiscalken@gmail.com>
2024-05-16 13:47:46 +0000

    prettified

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,22 +1,27 @@
 import { Flashcard } from '$lib/db';
-import type { PageServerLoad } from './$types';
+import type { Actions, PageServerLoad } from './$types';
 
-export const load = (async ({depends}) => {
-	const flashcards = (await Flashcard.find({}, {
-		question:true,
-		answer: true,
-		subject:true,
-		box: true
-	})).map((item) => {
+export const load = (async ({ depends }) => {
+	const flashcards = (
+		await Flashcard.find(
+			{},
+			{
+				question: true,
+				answer: true,
+				subject: true,
+				box: true
+			}
+		)
+	).map((item) => {
 		return {
 			question: item.question,
 			answer: item.answer,
 			subject: item.subject,
 			box: item.box,
 			_id: item._id.toString()
-		}
-	})
-	depends("app:root")
+		};
+	});
+	depends('app:root');
 	return {
 		flashcards
 	};
