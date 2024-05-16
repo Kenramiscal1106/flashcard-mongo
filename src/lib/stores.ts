cf9c311d42f5ae5ca0e59538671efaa2adeaa5b2
Ken Daniele Ramiscal <code.ramiscalken@gmail.com>
2024-05-16 13:46:14 +0000

    add edit field

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,31 +1,33 @@
-import { writable } from "svelte/store";
+import { writable } from 'svelte/store';
 
 export const BoxOptions = [
-    "everyday",
-    "1-2 days",
-    "4-7 days",
-    "1 1/2 week",
-    "2 weeks",
-    "1 month"
-] as const
+	'everyday',
+	'1-2 days',
+	'4-7 days',
+	'1 1/2 week',
+	'2 weeks',
+	'1 month'
+] as const;
 
 export type ModalState = {
-    open: boolean,
-    flashcard: {
-        _id: string,
-        subject:string,
-        box?: typeof BoxOptions[number],
-        question: string,
-        answer: string
-    }
-}
+	open: boolean;
+	flashcard: {
+		_id: string;
+		subject: string;
+		box?: (typeof BoxOptions)[number];
+		question: string;
+		answer: string;
+	};
+	mode: 'edit' | 'delete';
+};
 
 export const modalStore = writable<ModalState>({
-    open: false,
-    flashcard: {
-        _id: "",
-        subject:"",
-        question:"",
-        answer: ""
-    }
-})
+	open: false,
+	flashcard: {
+		_id: '',
+		subject: '',
+		question: '',
+		answer: ''
+	},
+	mode: 'edit'
+});
