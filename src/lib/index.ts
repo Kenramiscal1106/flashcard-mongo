d761a45c4e4dd06dccc804a63bdc46ddfd6d5092
Ken Daniele Ramiscal <code.ramiscalken@gmail.com>
2024-05-16 13:45:50 +0000

    add subjects

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,20 +1,24 @@
 // place files you want to import through the `$lib` alias in this folder.
 export async function shuffle<T>(arr: T[]) {
-    let finalArr = new Array<T>();
-    while (arr.length > 0) {
-      let randomIndex = Math.floor(Math.random() * arr.length);
-      finalArr.push(arr[randomIndex]);
-      arr.splice(randomIndex, 1);
-    }
-    return finalArr;
-  }
+	let finalArr = new Array<T>();
+	while (arr.length > 0) {
+		let randomIndex = Math.floor(Math.random() * arr.length);
+		finalArr.push(arr[randomIndex]);
+		arr.splice(randomIndex, 1);
+	}
+	return finalArr;
+}
 
 export const subjects = [
-  "Research",
-  "Science",
-  "Entrep",
-  "Rewrite",
-  "Theology",
-  "FiliTwo",
-  "Infolit"
-] as const
+	'Biology',
+	'Physics',
+	'Chemistry',
+	'Earth Science, Astronomy',
+	'Research',
+	'Science',
+	'Entrep',
+	'Rewrite',
+	'Theology',
+	'FiliTwo',
+	'Infolit'
+] as const;
