118e221e433aaf6fb2e5b6af6477978d1257ebfa
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2024-09-04 15:54:02 +0800

    change template

--- src/routes/add/+page.svelte
+++ src/routes/add/+page.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 </script>
 
-<main>
+<div class="max-w-full lg:max-w-4xl xl:max-w-5xl sm:max-w-2xl m-auto">
 	<a href="/add/card">Flashcard</a>
 	<a href="/add/subject">Subject</a>
-</main>
+</div>
 
 <style>
 </style>
