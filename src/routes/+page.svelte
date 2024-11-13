44cdc6696c68ad17ccf359e5944756fe5bb5f56b
Ken Daniele A. Ramiscal <89060408+Kenramiscal1106@users.noreply.github.com>
2024-11-13 18:20:09 +0800

    add icon and title to dashboard

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
 	import { subjects } from '$lib';
 </script>
 
+<svelte:head>
+	<title>Dashboard | Flashcard review</title>
+</svelte:head>
+
 <div class="max-w-full lg:max-w-4xl xl:max-w-5xl sm:max-w-2xl m-auto">
 	{#each subjects as subject}
 		<a href={`/cards/${subject.toLowerCase()}`}>{subject}</a> <br />
